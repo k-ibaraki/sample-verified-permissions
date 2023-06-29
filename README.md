1600ad56ce8aa3960a260dc4e1626bf5c9e0680f
k-ibaraki <ibaraki@ncdc.co.jp>
2023-06-29 13:38:11 +0900

    とりあえず、ハロワ

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sample-verified-permissions
+
+Verified Permissions を試すために適当に作ったレポジトリー
+
+## インストール
+```sh
+npm install
+```
+
+## 起動
+```sh
+npm run start
+```
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import expressOasGenerator from 'express-oas-generator'
+
+const app: express.Express = express()
+
+// CORSの許可
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+// body-parserに基づいた着信リクエストの解析
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const router: express.Router = express.Router()
+
+// hello world
+router.get('/', (req: express.Request, res: express.Response) => {
+  res.send("Hello World")
+})
+
+app.use(router)
+
+// express-oas-generator : OpenAPIドキュメントの自動生成
+expressOasGenerator.init(app, {})
+
+// 3000番ポートでAPIサーバ起動
+app.listen(3000, () => { console.log('Example app listening on port 3000!') })
