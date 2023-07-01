f24ee571532446219751def36cd661c9b6739881
k-ibaraki <ibaraki@ncdc.co.jp>
2023-07-01 11:31:34 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import expressOasGenerator from 'express-oas-generator'
-import { vfTest } from './verifiedPermissionsSample'
+import {
+  verifiedPermissionsAuth
+} from './verifiedPermissionsSample'
 import {
   PORT,
 } from './constants'
@@ -13,32 +15,22 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
-})
+});
 
 // body-parserに基づいた着信リクエストの解析
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const router: express.Router = express.Router()
+const router: express.Router = express.Router();
 
 // hello world
-router.get('/hello', async (req: express.Request, res: express.Response) => {
-  const idToken = req.headers.authorization?.replace("Bearer ", "") ?? "";
-  try {
-    if (await vfTest(idToken, "hello", "Get")) {
-      res.send("Hello World");
-    } else {
-      res.status(403).send("Not Authorized");
-    }
-  } catch (err: any) {
-    res.status(400).send(err.message);
-  }
-})
-
-app.use(router)
+router.get('/hello', verifiedPermissionsAuth, async (req: express.Request, res: express.Response) => {
+  res.send("Hello World");
+});
+app.use(router);
 
 // express-oas-generator : OpenAPIドキュメントの自動生成
-expressOasGenerator.init(app, {})
+expressOasGenerator.init(app, {});
 
 // 3000番ポートでAPIサーバ起動
 app.listen(PORT, () => { console.log(`http://localhost:${PORT}`) })
--- src/verifiedPermissionsSample.ts
+++ src/verifiedPermissionsSample.ts
@@ -4,8 +4,31 @@ import {
   VP_ACTION_TYPE,
   VP_RESOURCE_TYPE
 } from './constants'
+import { Request, Response, NextFunction } from 'express'
 
-export const vfTest = async (idToken: string, resource: string, action: string): Promise<boolean> => {
+export const verifiedPermissionsAuth = async (req: Request, res: Response, next: NextFunction) => {
+  // AuthorizationヘッダからidTokenを取得
+  const idToken = req.headers.authorization?.replace("Bearer ", "") ?? "";
+  // Pathを取得(/は除く)
+  const resource = req.path.replace("/", "");
+  // Methodを取得
+  const action = req.method;
+  // 認可判定
+  try {
+    if (await isAuthorized(idToken, resource, action)) {
+      // 認可OK
+      next();
+    } else {
+      // 認可NG
+      res.status(403).send("Not Authorized");
+    }
+  } catch (e: any) {
+    // なにかエラーが出たら401
+    res.status(401).send(e.message);
+  }
+}
+
+const isAuthorized = async (idToken: string, resource: string, action: string): Promise<boolean> => {
   const client = new VerifiedPermissionsClient({ region: "ap-northeast-1" });
   const command = new IsAuthorizedWithTokenCommand({
     policyStoreId: VP_POLICY_STORE_ID,
